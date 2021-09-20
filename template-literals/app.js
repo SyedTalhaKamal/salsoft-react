d08b7397545d8ba8587ad52402b8bcaae741ba85
unknown <syedtalhakamal22@gmail.com>
2021-09-20 12:37:29 -0700

    Template Literals

--- template-literals/app.js
+++ template-literals/app.js
@@ -1,43 +1,33 @@
 let humans = [{
-        name: "azam",
-        age: 18,
-        gender: "Male",
-        img: "images/chapter-1.png",
-    },
-    {
-        name: "haseeb",
-        age: 25,
-        gender: "Male",
-        img: "images/chapter-2.png",
-    },
-    {
-        name: "ahab",
-        age: 26,
-        gender: "Male",
-        img: "images/chapter-3.png",
-    },
+    name: "azam",
+    age: 18,
+    gender: "Male",
+    img: "images/chapter-1.png",
+},
+{
+    name: "haseeb",
+    age: 25,
+    gender: "Male",
+    img: "images/chapter-2.png",
+},
+{
+    name: "ahab",
+    age: 26,
+    gender: "Male",
+    img: "images/chapter-3.png",
+},
 ];
 
 let container = document.getElementById("container");
 let templateList;
 humans.forEach(human => {
 
-     templateList = `<ul class="list">
+    templateList = `<ul class="list">
         <li>${human.name}</li>
+        <li>${human.age}</li>
+        <li>${human.gender}</li>
         </ul>
         `;
     console.log(templateList);
+    container.innerHTML = container.innerHTML + templateList;
 });
-
-
-//   let templateList = `<ul class="list">
-//   ${humans.forEach((human) => {
-//     `<li>Hello</li>`;
-//    ` <li>${human.name}</li>;
-//     <li>${human.age}</li>;
-//     <li>${human.gender}</li>;`
-//   })}
-//   </ul>`;
-//   console.log(templateList);
-
-  container.innerHTML = templateList;
